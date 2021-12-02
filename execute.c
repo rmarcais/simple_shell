dcdf9af9ad39623e219b822a727403238f032921
julien3641 <3641@holbertonschool.com>
2021-12-02 16:27:19 +0100

    add comments on functions

--- execute.c
+++ execute.c
@@ -1,4 +1,11 @@
 #include "simple_shell.h"
+
+/**
+ * execute - This function execute the function given by the user
+ * @av: take the array that contain the command of the user
+ * Return: Return 0 on succes or -1 on error
+ */
+
 int execute(char **av)
 {
 	int pid = fork();
